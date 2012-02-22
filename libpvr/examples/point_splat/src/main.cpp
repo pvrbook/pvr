d2e30389308e404db383955e3c231de3c74d5d19
Magnus Wrenninge <magnus.wrenninge@gmail.com>
2012-02-21 20:08:55 -0800

    Updated test app point_splat

--- libpvr/examples/point_splat/src/main.cpp
+++ libpvr/examples/point_splat/src/main.cpp
@@ -1,6 +1,6 @@
 //-*-c++-*--------------------------------------------------------------------//
 
-/* This example shows some use of the AttrTable and related classes.
+/* 
  * 
  * 
  *
@@ -19,6 +19,7 @@
 #include <Field3D/DenseField.h>
 #include <Field3D/DenseField.h>
 
+#include <pvr/Image.h>
 #include <pvr/VoxelBuffer.h>
 #include <pvr/ModelingUtils.h>
 
@@ -31,24 +32,92 @@ using namespace Imath;
 using namespace Field3D;
 using namespace pvr;
 using namespace pvr::Model;
+using namespace pvr::Render;
+
+//----------------------------------------------------------------------------//
+
+void writeSliceAsImage(DenseBuffer::Ptr buffer, int res, int slice, 
+                       const std::string &filename)
+{
+  Image::Ptr image = Image::create();
+  image->setSize(res, res);
+  for (Image::pixel_iterator i = image->begin(), end = image->end();
+       i != end; ++i) {
+    i.setPixel(buffer->value(i.x, i.y, slice));
+  }
+  image->write(filename, Image::RGB);
+}
 
 //----------------------------------------------------------------------------//
 
 int main()
 {
+  int res = 200;
+  int numPoints = 500; 
+
+  Imath::Rand32 rng(0);
+
   DenseBuffer::Ptr buffer(new DenseBuffer);
-  buffer->setSize(V3i(5));
+  buffer->setSize(V3i(res));
   buffer->clear(V3f(0.0));
 
-  cout << "writePoint()" << endl;
-  writePoint(Vector(0.5), V3f(1.0), buffer);
-  cout << "  " << buffer->value(0, 0, 0) << endl;
+  for (int i = 0; i < numPoints; i++) {
+    float x = rng.nextf() * res;
+    float y = rng.nextf() * res;
+    float z = 0.5;
+    writePoint(Vector(x, y, z), V3f(1.0), buffer);
+  }
+  writeSliceAsImage(buffer, res, 0, "nonantialiased.png");
+  
+  buffer->clear(V3f(0.0));
+  rng.init(0);
+
+  for (int i = 0; i < numPoints; i++) {
+    float x = rng.nextf() * res;
+    float y = rng.nextf() * res;
+    float z = 0.5;
+    writeAntialiasedPoint(Vector(x, y, z), V3f(1.0), buffer);
+  }
+  writeSliceAsImage(buffer, res, 0, "antialiased.png");
+  
+  buffer->clear(V3f(0.0));
+  rng.init(0);
+
+  for (int i = 0; i < numPoints; i++) {
+    float x = rng.nextf() * res;
+    float y = rng.nextf() * res;
+    float z = 0.5;
+    float xe = x + (rng.nextf() - 0.5) * 0.1 * res;
+    float ye = y + (rng.nextf() - 0.5) * 0.1 * res;
+    writeLine<true>(Vector(x, y, z), Vector(xe, ye, z), V3f(1.0), buffer);
+  }
+  writeSliceAsImage(buffer, res, 0, "lines.png");
+  
+  // High point count
+
+  numPoints = numPoints * 1000; 
+  float density = 1.0 / 100;
+
+   buffer->clear(V3f(0.0));
+
+  for (int i = 0; i < numPoints; i++) {
+    float x = rng.nextf() * res;
+    float y = rng.nextf() * res;
+    float z = 0.5;
+    writePoint(Vector(x, y, z), V3f(density), buffer);
+  }
+  writeSliceAsImage(buffer, res, 0, "nonantialiased-hi.png");
 
   buffer->clear(V3f(0.0));
-  cout << "writeAntialiasedPoint()" << endl;
-  writeAntialiasedPoint(Vector(0.7, 0.5, 0.5), V3f(1.0), buffer);
-  cout << "  " << buffer->value(0, 0, 0) << endl
-       << "  " << buffer->value(1, 0, 0) << endl;
+  rng.init(0);
+
+  for (int i = 0; i < numPoints; i++) {
+    float x = rng.nextf() * res;
+    float y = rng.nextf() * res;
+    float z = 0.5;
+    writeAntialiasedPoint(Vector(x, y, z), V3f(density), buffer);
+  }
+  writeSliceAsImage(buffer, res, 0, "antialiased-hi.png"); 
 }
 
 //----------------------------------------------------------------------------//
