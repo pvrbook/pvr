d69595175412b92214a38a5037e3e0f5a7672ca3
Magnus Wrenninge <magnus.wrenninge@gmail.com>
2012-07-08 18:43:19 -0700

    Added GPL3 copyright notice.

--- libpvr/export/RaymarchSamplers/PhysicalSampler.h
+++ libpvr/export/RaymarchSamplers/PhysicalSampler.h
@@ -1,5 +1,24 @@
 //-*-c++-*--------------------------------------------------------------------//
 
+/*
+    This file is part of PVR. Copyright (C) 2012 Magnus Wrenninge
+
+    PVR is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PVR is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//----------------------------------------------------------------------------//
+
 /*! \file PhysicalSampler.h
   Contains the PhysicalSampler class and related functions.
  */
