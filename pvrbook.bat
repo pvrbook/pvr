f3b92d6da8091bea9667bdb1a5373d3f88475574
darkhorse64 <henri.fousse@free.fr>
2013-01-31 21:36:48 +0100

    VisualStudio2010 solution

--- pvrbook.bat
+++ pvrbook.bat
@@ -1,2 +1,3 @@
+rem  the THIRD_PARTY_TOOLS_HOME environment variable defines where dependencies (includes, libs) for PVR are stored
 set THIRD_PARTY_TOOLS_HOME=d:\Third party tools\dist
 vs2010\pvrbook.sln
